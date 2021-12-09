7de3b8a2e850a3f73c52e3e15162be9517403487
CleideS <40967363+cleides@users.noreply.github.com>
2021-12-09 21:52:12 +0000

    Exercise002.Final

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -1,15 +1,23 @@
 package com.techreturners.exercise002;
 
+import javax.swing.*;
+
 public class Exercise002 {
 
-    public boolean isFromManchester(/* Person person */) {
-        // Add your code here
-        return false;
+    public boolean isFromManchester(Person person) {
+
+        if (person.city.equals("Manchester")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
-        // Add your code here
-        return false;
+    public boolean canWatchFilm(Person person, int ageLimit) {
+        if (person.age >= 18) {
+            return true;
+        } else {
+            return false;
+        }
     }
-    
 }
--- src/main/java/com/techreturners/exercise002/Person.java
+++ src/main/java/com/techreturners/exercise002/Person.java
@@ -1,4 +1,24 @@
 package com.techreturners.exercise002;
 
 public class Person {
+    public String firstName;  // properties
+    public String lastName;
+    public String city;
+    public int age;
+
+    public void printData() {
+        System.out.println(firstName);    // methods
+        System.out.println(lastName);
+        System.out.println(city);
+        System.out.println(age);
+    }
+
+    //constructor
+    public Person(String firstName, String lastName, String city, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.age = age;
+    }
+
 }
