d66e9418a1ee280b0e93ba6f741b59ff918b43b2
CleideS <40967363+cleides@users.noreply.github.com>
2021-12-11 14:16:18 +0000

    Exercise003.partial

--- src/main/java/com/techreturners/exercise003/Exercise003.java
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -3,7 +3,13 @@ package com.techreturners.exercise003;
 public class Exercise003 {
 
     int getIceCreamCode(String iceCreamFlavour) {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        String[] icecreams = {"Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet" };
+        for (int i = 0; i < icecreams.length; i++) {
+            if(iceCreamFlavour.equals(icecreams[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     String[] iceCreamFlavours() {
@@ -11,3 +17,4 @@ public class Exercise003 {
     }
 
 }
+
