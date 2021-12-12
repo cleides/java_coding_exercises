040f3d9841ca28ba65d10205ef772a6e6d4cf98d
CleideS <40967363+cleides@users.noreply.github.com>
2021-12-12 15:41:01 +0000

    Exercise005.java.Init

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -1,9 +1,32 @@
 package com.techreturners.exercise005;
 
 public class Exercise005 {
+    char[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-    public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+    public boolean isPangram(String inputToBeChecked) {
+
+        inputToBeChecked = inputToBeChecked.toLowerCase();
+
+        for (int i = 0; i < inputToBeChecked.length(); i++) {
+            char character = inputToBeChecked.charAt(i);
+            searchLettersAndBlankLetterIfFound(character);
+        }
+
+        boolean result = areLettersAllSpaces();
+        return result;
     }
 
+    private boolean areLettersAllSpaces() {
+
+        return true;
+    }
+
+    private void searchLettersAndBlankLetterIfFound(char character) {
+
+        for (int i = 0; i < letters.length; i++) {
+            if (character == (letters[i])) {
+                letters[i] = ' ';
+            }
+        }
+    }
 }
